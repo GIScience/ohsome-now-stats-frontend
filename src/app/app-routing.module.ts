3df281eee7f9c826ac6c1337e9072f82ded2e5db
rizwan <rizwan@uni-heidelberg.de>
2023-06-13 16:34:42 +0200

    refactor: remove unwanted resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RouteResolver } from './resolvers/route.resolver';
 
 const routes: Routes = [
   { 
@@ -12,10 +11,7 @@ const routes: Routes = [
   },
   { 
     path: 'dashboard', 
-    component: DashboardComponent,
-    resolve: {
-      summaryData: RouteResolver
-    }
+    component: DashboardComponent
   }
 ];
 
