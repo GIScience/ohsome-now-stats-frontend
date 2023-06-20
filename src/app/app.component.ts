10e3fd5d786edba6ecf4cbeb7e6cdc3e49d45ddf
rizwan <rizwan@uni-heidelberg.de>
2023-06-20 16:26:34 +0200

    refactor: decouples sidebar functionality with main dashboard #12
    each points in the leaft sidebar will be pointing to new route and its content are seperate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,42 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dashboard - ohsome Contribution Statistics';
   name = 'HeiGIT';
+
+  isOpen = false
+  activeLink = ''
+
+  constructor() {}
+
+  toggleSidebar() {
+    console.log('>>> AppComponent >>> toggleSidebar ')
+    this.isOpen = !this.isOpen
+    // this.triggerResizeEvent()
+    const app = document.querySelector('.app')
+    if(app){
+      if(app.classList.contains('is-collapsed')) 
+        app.classList.remove('is-collapsed')
+      else
+        app.classList.add('is-collapsed')
+    }
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault()
+    const clickedLink = event.target as HTMLElement
+    const parentListItem = clickedLink.parentElement
+
+    if(parentListItem)
+    if (parentListItem.classList.contains('open')) {
+      parentListItem.classList.remove('open')
+    } else {
+      const openLinks = document.querySelectorAll('.sidebar .sidebar-menu li.open')
+      openLinks.forEach((openLink) => {
+        openLink.classList.remove('open')
+      })
+    
+      parentListItem.classList.add('open')
+    }
+
+  }
+
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
-import { AfterContentInit, Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 
 import { DataService, ISummaryData } from '../data.service';
 
@@ -8,70 +8,99 @@ import { DataService, ISummaryData } from '../data.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit, AfterContentInit {
+export class DashboardComponent implements OnInit {
 
   title = 'ohsome-contribution-stats'
   isOpen = false
   activeLink = ''
   summaryData: ISummaryData | undefined
-  plotData: any = [{
-    "changesets": 65009011,
-    "users": 3003842,
-    "roads": 45964973.0494135,
-    "buildings": 142,
-    "edits": 1095091515,
+  plotData : any = [{
+    "changesets": 18339.0,
+    "users": 728.0,
+    "roads": 3583436.881372036,
+    "buildings": 116093.0,
+    "edits": 167979.0,
     "latest": "2023-03-20T10:55:38.000Z",
-    "hashtag": "*",
-    "enddate": "2017-10-01T00:00:00.000Z",
-    "startdate": "2017-09-01T00:00:00.000Z"
-  },{
-    "changesets": 65009011,
-    "users": 3003842,
-    "roads": 45964973.0494135,
-    "buildings": 210,
-    "edits": 1095091515,
+    "hashtag": "#MissingMaps",
+    "startdate": "2014-01-01T00:00:00.000Z",
+    "enddate": "2015-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 132676.0,
+    "users": 4867.0,
+    "roads": 69958845.22928844,
+    "buildings": 1126030.0,
+    "edits": 1594605.0,
     "latest": "2023-03-20T10:55:38.000Z",
-    "hashtag": "*",
-    "enddate": "2017-11-01T00:00:00.000Z",
-    "startdate": "2017-10-01T00:00:00.000Z"
-  },{
-    "changesets": 65009011,
-    "users": 3003842,
-    "roads": 45964973.0494135,
-    "buildings": 402,
-    "edits": 1095091515,
+    "hashtag": "#MissingMaps",
+    "startdate": "2015-01-01T00:00:00.000Z",
+    "enddate": "2016-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 439255.0,
+    "users": 16510.0,
+    "roads": 248230227.0966366,
+    "buildings": 5337828.0,
+    "edits": 7273160.0,
     "latest": "2023-03-20T10:55:38.000Z",
-    "hashtag": "*",
-    "enddate": "2018-10-01T00:00:00.000Z",
-    "startdate": "2018-09-01T00:00:00.000Z"
-  },{
-    "changesets": 65009011,
-    "users": 3003842,
-    "roads": 45964973.0494135,
-    "buildings": 432,
-    "edits": 1095091515,
+    "hashtag": "#MissingMaps",
+    "startdate": "2016-01-01T00:00:00.000Z",
+    "enddate": "2017-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 1113237.0,
+    "users": 28386.0,
+    "roads": 342925140.3880086,
+    "buildings": 18673461.0,
+    "edits": 22277239.0,
     "latest": "2023-03-20T10:55:38.000Z",
-    "hashtag": "*",
-    "enddate": "2018-11-01T00:00:00.000Z",
-    "startdate": "2018-10-01T00:00:00.000Z"
-  },{
-    "changesets": 65009011,
-    "users": 3003842,
-    "roads": 45964973.0494135,
-    "buildings": 442,
-    "edits": 1095091515,
+    "hashtag": "#MissingMaps",
+    "startdate": "2017-01-01T00:00:00.000Z",
+    "enddate": "2018-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 574783.0,
+    "users": 29049.0,
+    "roads": 223979939.61303145,
+    "buildings": 14554788.0,
+    "edits": 17329823.0,
     "latest": "2023-03-20T10:55:38.000Z",
-    "hashtag": "*",
-    "enddate": "2018-11-23T12:23:00.000Z",
-    "startdate": "2018-11-01T00:00:00.000Z"
+    "hashtag": "#MissingMaps",
+    "startdate": "2018-01-01T00:00:00.000Z",
+    "enddate": "2019-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 459575.0,
+    "users": 27420.0,
+    "roads": 186701467.78327608,
+    "buildings": 9170816.0,
+    "edits": 10879559.0,
+    "latest": "2023-03-20T10:55:38.000Z",
+    "hashtag": "#MissingMaps",
+    "startdate": "2019-01-01T00:00:00.000Z",
+    "enddate": "2020-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 451801.0,
+    "users": 29193.0,
+    "roads": 81130653.12457849,
+    "buildings": 9586938.0,
+    "edits": 11140997.0,
+    "latest": "2023-03-20T10:55:38.000Z",
+    "hashtag": "#MissingMaps",
+    "startdate": "2020-01-01T00:00:00.000Z",
+    "enddate": "2021-01-01T00:00:00.000Z"
+  }, {
+    "changesets": 629192.0,
+    "users": 35542.0,
+    "roads": 85310718.5236433,
+    "buildings": 14242294.0,
+    "edits": 16510855.0,
+    "latest": "2023-03-20T10:55:38.000Z",
+    "hashtag": "#MissingMaps",
+    "startdate": "2021-01-01T00:00:00.000Z",
+    "enddate": "2022-01-01T00:00:00.000Z"
   }]
   queryParams: any;
   summaryMessage: string = '';
 
   constructor(
     private dataService: DataService,
-    private route: ActivatedRoute,
-    private router: Router ) {}
+    private route: ActivatedRoute ) {}
 
   ngOnInit() {
     // const startDate = this.route.snapshot.paramMap.get('id')
@@ -100,42 +129,6 @@ export class DashboardComponent implements OnInit, AfterContentInit {
 
     })
     
-  }
-
-  toggleSidebar() {
-    console.log('>>> AppComponent >>> toggleSidebar ')
-    this.isOpen = !this.isOpen
-    // this.triggerResizeEvent()
-    const app = document.querySelector('.app')
-    if(app){
-      if(app.classList.contains('is-collapsed')) 
-        app.classList.remove('is-collapsed')
-      else
-        app.classList.add('is-collapsed')
-    }
-  }
-
-  toggleDropdown(event: Event) {
-    event.preventDefault()
-    const clickedLink = event.target as HTMLElement
-    const parentListItem = clickedLink.parentElement
-
-    if(parentListItem)
-    if (parentListItem.classList.contains('open')) {
-      parentListItem.classList.remove('open')
-    } else {
-      const openLinks = document.querySelectorAll('.sidebar .sidebar-menu li.open')
-      openLinks.forEach((openLink) => {
-        openLink.classList.remove('open')
-      })
-    
-      parentListItem.classList.add('open')
-    }
-
-  }
-
-  ngAfterContentInit(): void {
-
   }
 
   getQueryParamsFromFragments(): any {
