c9f14803e929448b879f6f7f6a80e764522a2432
Benjamin Herfort <Hagellach37@users.noreply.github.com>
2023-07-27 22:10:18 +0200

    Update tooltip-data.ts
    simpler tooltip information for statistics and trending hashtags. more details will be provided on the help page.

--- src/app/dashboard/tooltip-data.ts
+++ src/app/dashboard/tooltip-data.ts
@@ -1,12 +1,12 @@
 export const dashboard = {
   summary: {
-    contributions: "The number of distinct users that edited OSM data within the given time range for the particular changeset comment hashtag.",
-    totalEdits: "This is the number of all OSM edits to \"primary map features\" within the given time range for the particular changeset comment hashtag. Accordingly, this considers only edits for OSM elements with the tag keys defined in the OSM Wiki as Map Features.<br><br>We consider edits to all OSM types (nodes, ways, relations) and the following OSM tag keys: aerialway, aeroway, amenity, barrier, boundary, building, craft, emergency, geological, healthcare, highway, historic, landuse, leisure, man_made, military, natural, office, place, power, public_transport, railway, route, shop, sport, telecom, tourism, water, waterway.",
-    buildings: "This is the number of buildings newly added to OSM within the given time range for the particular changeset comment hashtag. <br><br>Buildings are defined as all OSM ways or relations which hold the tag key building and one of the following tag values: yes, house, residential, detached, detached, garage, apartments, shed, hut, industrial.",
-    kmOfRoads: "This is the length of roads in kilometers newly added to OSM and also considers the change in length when the geometry of existing roads is modified within the given time range for the particular changeset comment hashtag.<br><br>Roads are defined as all OSM ways or relations which hold the tag key highway and one of the following tag values: motorway, trunk, motorway_link, trunk_link, primary, primary_link, secondary, secondary_link, tertiary, tertiary_link, unclassified, residential.",
+    contributions: "The number of distinct users that edited OSM data for the hashtag and the time range defined in your query.",
+    totalEdits: "The number of all OSM edits to "primary map features" for the hashtag and the time range defined in your query.",
+    buildings: "The absolute increase or decrease in the amount of buildings in the OSM database induced by mapping activity for the hashtag and the time range defined in your query.",
+    kmOfRoads: "The absolute increase or decrease in the road network length in kilometers in the OSM database induced by mapping activity for the hashtag and the time range defined in your query.",
   },
   trendingHashtag: {
-    title: "This is a list of the 10 most used changeset comment hashtags within the given time range utilizing the number of distinct OSM users as a measure to sort.",
+    title: "The trending hashtags leaderboard lists the 10 most used changeset comment hashtags for the time range defined in your query. The first entry in the lists represents the hashtag with the highest number of distinct OSM contributors.",
     hashtags: "Hashtags are used in OSM as a popular way to mark changesets which belong to a particular mapping event, larger campaign or dedicated group of contributors.<br><br>The extracted hashtags are case-insensitive. This means that when you are adding #missingmaps or #MissingMaps or #missingMaps to a OSM changeset comment this will all be counted towards lower case missingmaps."
   }
 }
