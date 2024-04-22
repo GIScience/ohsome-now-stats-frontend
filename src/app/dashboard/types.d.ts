8d63ac27bb2fe47aa21f8f5c9e758568823c3c4e
jstier <stier.jochen@web.de>
2024-04-22 13:32:24 +0200

    feat: make commas consistent

--- src/app/dashboard/types.d.ts
+++ src/app/dashboard/types.d.ts
@@ -44,14 +44,14 @@ export interface IWrappedTopicData {
 }
 
 export interface TopicDefinitionValue {
-    id: string,
-    name: string,
-    "color-hex": string,
-    "color-light": string,
-    "y-title": string,
-    dropdown_name?: string,
-    tooltip?: string,
-    icon?: string,
+    id: string
+    name: string
+    "color-hex": string
+    "color-light": string
+    "y-title": string
+    dropdown_name?: string
+    tooltip?: string
+    icon?: string
     value?: string
 }
 
@@ -62,16 +62,16 @@ export interface IWrappedSummaryData {
 }
 
 export interface ISummaryData {
-    changesets?: number,
+    changesets?: number
     users: number
     edits: number
     buildings: number
-    roads: number,
-    latest?: string,
-    hashtag?: string,
-    countries?: string,
-    startDate?: string,
-    endDate?: string,
+    roads: number
+    latest?: string
+    hashtag?: string
+    countries?: string
+    startDate?: string
+    endDate?: string
 }
 
 export interface IQueryData {
@@ -88,15 +88,15 @@ export interface IWrappedPlotData {
 }
 
 export interface IPlotData {
-    changesets?: number[],
-    users: number[],
-    roads: number[],
-    buildings: number[],
-    edits: number[],
-    latest?: string,
-    hashtag?: string | string[],
-    startDate: string[],
-    endDate: string[],
+    changesets?: number[]
+    users: number[]
+    roads: number[]
+    buildings: number[]
+    edits: number[]
+    latest?: string
+    hashtag?: string | string[]
+    startDate: string[]
+    endDate: string[]
     countries?: string | string[]
 }
 
@@ -106,9 +106,9 @@ export interface IWrappedTopicPlotData {
 }
 
 export interface ITopicPlotData {
-    value: number[],
-    topic: string,
-    startDate: string[],
+    value: number[]
+    topic: string
+    startDate: string[]
     endDate: string[]
 }
 
@@ -118,8 +118,8 @@ export interface IWrappedTopicCountryData {
 }
 
 export interface ITopicCountryData {
-    topic: string,
-    country: string,
+    topic: string
+    country: string
     value: number
 }
 
@@ -133,59 +133,59 @@ export interface IWrappedCountryStatsData {
 }
 
 export interface ICountryStatsData {
-    users: number,
-    roads: number,
-    buildings: number,
-    edits: number,
-    'amenity'?: number,
-    'commercial'?: number,
-    'education'?: number,
-    'financial'?: number,
-    'healthcare'?: number,
-    'lulc'?: number,
-    'place'?: number,
-    'poi'?: number,
-    'social_facility'?: number,
-    'wash'?: number,
-    'waterway'?: number,
-    latest: string,
+    users: number
+    roads: number
+    buildings: number
+    edits: number
+    'amenity'?: number
+    'commercial'?: number
+    'education'?: number
+    'financial'?: number
+    'healthcare'?: number
+    'lulc'?: number
+    'place'?: number
+    'poi'?: number
+    'social_facility'?: number
+    'wash'?: number
+    'waterway'?: number
+    latest: string
     country: string
 }
 
 export interface IQueryParam {
-    countries: string,
-    hashtag: string,
-    start: string, // date in ISO format, ensure to keep milliseconds as 0
-    end: string, // date in ISO format, ensure to keep milliseconds as 0
-    interval: string, // eg:'P1D' default value: 'P1M'
+    countries: string
+    hashtag: string
+    start: string // date in ISO format, ensure to keep milliseconds as 0
+    end: string // date in ISO format, ensure to keep milliseconds as 0
+    interval: string // eg:'P1D' default value: 'P1M'
     topics: string
 }
 
 export interface IHashtag {
-    hashtagTitle: string,
-    hashtag: string,
-    number_of_users: number,
-    tooltip: string,
+    hashtagTitle: string
+    hashtag: string
+    number_of_users: number
+    tooltip: string
     percent: number
 }
 
 export interface IMetaData {
-    start: string, // date in ISO format, ensure to keep milliseconds as 0
-    end: string, // date in ISO format, ensure to keep milliseconds as 0
+    start: string // date in ISO format, ensure to keep milliseconds as 0
+    end: string // date in ISO format, ensure to keep milliseconds as 0
 }
 
 interface IHashtags {
-    hashtag: string,
+    hashtag: string
     count: number
 }
 
 
 interface IHighlightedHashtag {
-    hashtag: string,
+    hashtag: string
     highlighted: string
 }
 
 interface IDateRange {
-    end: dayjs.Dayjs;
-    start: dayjs.Dayjs;
+    end: dayjs.Dayjs
+    start: dayjs.Dayjs
 }
