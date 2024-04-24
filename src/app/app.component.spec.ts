f3fa1dcbc25f039f3f1a51f4ecbb4f2fe6a6ea6d
jstier <stier.jochen@web.de>
2024-04-24 13:34:29 +0200

    fix: missing import in test
    Co-authored-by: BerendvanTilburg <bcavantilburg@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { ToastComponent } from './toast/toast.component';
-import { SummaryComponent } from './dashboard/summary/summary.component';
-import { QueryComponent } from './dashboard/query/query.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TrendingHashtagsComponent } from './dashboard/trending-hashtags/trending-hashtags.component';
-import { DataService } from './data.service';
-import { BigNumberComponent } from './dashboard/summary/big-number/big-number.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {ToastComponent} from './toast/toast.component';
+import {SummaryComponent} from './dashboard/summary/summary.component';
+import {QueryComponent} from './dashboard/query/query.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TrendingHashtagsComponent} from './dashboard/trending-hashtags/trending-hashtags.component';
+import {DataService} from './data.service';
+import {BigNumberComponent} from './dashboard/summary/big-number/big-number.component';
 import {ExportDataComponent} from "./dashboard/export-data/export-data.component";
 import {PlotComponent} from "./dashboard/plot/plot.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
@@ -17,47 +17,49 @@ import {HelpComponent} from "./help/help.component";
 import {UTCToLocalConverterPipe} from "./dashboard/query/pipes/utc-to-local-converter.pipe";
 import {Overlay} from "./overlay.component";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {StatusBannerComponent} from "./status-banner/status-banner.component";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientTestingModule
-      ],
-      declarations: [
-        AppComponent,
-        SummaryComponent,
-        QueryComponent,
-        PlotComponent,
-        DashboardComponent,
-        PageNotFoundComponent,
-        MapComponent,
-        TrendingHashtagsComponent,
-        ToastComponent,
-        AboutComponent,
-        HelpComponent,
-        BigNumberComponent,
-        ExportDataComponent,
-        UTCToLocalConverterPipe,
-        Overlay
-      ],
-      providers: [
-        DataService
-      ]
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                HttpClientTestingModule
+            ],
+            declarations: [
+                AppComponent,
+                SummaryComponent,
+                QueryComponent,
+                PlotComponent,
+                DashboardComponent,
+                PageNotFoundComponent,
+                MapComponent,
+                TrendingHashtagsComponent,
+                ToastComponent,
+                AboutComponent,
+                HelpComponent,
+                BigNumberComponent,
+                ExportDataComponent,
+                UTCToLocalConverterPipe,
+                Overlay,
+                StatusBannerComponent
+            ],
+            providers: [
+                DataService
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'ohsomeNow Stats'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ohsomeNow Stats');
-  });
-  
+    it(`should have as title 'ohsomeNow Stats'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('ohsomeNow Stats');
+    });
+
 });
