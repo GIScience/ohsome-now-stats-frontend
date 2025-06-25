3449af6062f2ee0c0c7478f4045436c0defdd37d
mrkhan <rizwan.khan@heigit.org>
2025-06-25 16:09:13 +0200

    fix: show legend scale from minValue to maxValue (positive number) for Contributors

--- src/app/dashboard/hex-map/hex-map.component.ts
+++ src/app/dashboard/hex-map/hex-map.component.ts
@@ -125,7 +125,7 @@ export class HexMapComponent implements OnInit, OnDestroy {
                 }),
                 {
                     result: {
-                        min: 0,
+                        min: 1,
                         max: 0
                     }
                 }
