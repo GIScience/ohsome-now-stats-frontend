GIScience/ohsome-now-stats-frontend

commit 5ffc504b4f219dfe2d7983062721d61ff6447786

feat: make signal unresponsive to interval changes

src/app/dashboard/hex-map/hex-map.component.ts
@@ -5,7 +5,7 @@ import {BitmapLayer} from '@deck.gl/layers';
 import {lch, rgb} from 'd3-color';
 import {scalePow} from 'd3-scale';
 import {interpolateHcl} from "d3-interpolate";
-import {HexDataType, StatsType} from '../types'
+import {HexDataType, IStateParams, StatsType} from '../types'
 import {StateService} from "../../state.service";
 import {DataService} from "../../data.service";
 import topicDefinitions from "../../../assets/static/json/topicDefinitions.json"
@@ -19,7 +19,17 @@ import topicDefinitions from "../../../assets/static/json/topicDefinitions.json"
 export class HexMapComponent implements OnInit, OnDestroy {
     @ViewChild('deckContainer', {static: true}) deckContainer!: ElementRef;
 
-    private relevantState = computed(() => this.stateService.appState())
+    private relevantState = computed((): IStateParams => {
+        return this.stateService.appState()
+    }, {
+        equal: (a, b) => {
+            return a.hashtag === b.hashtag
+                && a.start === b.start
+                && a.end === b.end
+                && a.topics == b.topics
+                && a.countries == b.countries
+        }
+    })
 
     selectedTopic!: StatsType;
     deck!: Deck;

commit b69d6a809c5b6b15c0baedb95708ec63c8f09f1d

fix: non responsive hex map
5ffc504b4f219dfe2d7983062721d61ff6447786 introduced a bug where hex map no longer updated on active topic switch

src/app/dashboard/hex-map/hex-map.component.html
@@ -27,7 +27,7 @@
         </div>
         <div *ngIf="!isH3Loading && minMaxStats" class="legend-container">
             <app-legend
-                    [colorFunction]="getColorFn()"
+                    [colorFunction]="colorFunc"
                     [maxValue]="minMaxStats.result.max"
                     [minValue]="minMaxStats.result.min"
                     [unit]="getTopicUnit()">

src/app/dashboard/hex-map/hex-map.component.ts
@@ -24,14 +24,15 @@ export class HexMapComponent implements OnInit, OnDestroy {
         return this.stateService.appState()
     }, {
         equal: (a, b) => {
-            return a.hashtag === b.hashtag
+            return a.active_topic === b.active_topic
+                && a.hashtag === b.hashtag
                 && a.start === b.start
                 && a.end === b.end
                 && a.topics == b.topics
                 && a.countries == b.countries
         }
     })
-
+    colorFunc: ((value: HexDataType) => Color) | undefined = undefined;
     selectedTopic!: StatsType;
     deck!: Deck;
     minMaxStats!: { result: { max: number; min: number } };
@@ -116,12 +117,19 @@ export class HexMapComponent implements OnInit, OnDestroy {
         } as any);
     }
 
-    async updateLayer(reqParams: { hashtag: string; start: string; end: string; countries: string; topic: string; resolution: number; }) {
+    async updateLayer(reqParams: {
+        hashtag: string;
+        start: string;
+        end: string;
+        countries: string;
+        topic: string;
+        resolution: number;
+    }) {
         this.layer = await this.createCountryLayer(reqParams);
         this.deck.setProps({
             layers: [this.osmLayer, this.layer],
             getTooltip: ({object}: PickingInfo<HexDataType>) =>
-                object ? { text: `${object.result} ${topicDefinitions[this.selectedTopic]?.["y-title"]}` } : null
+                object ? {text: `${object.result} ${topicDefinitions[this.selectedTopic]?.["y-title"]}`} : null
         });
     }
 
@@ -136,7 +144,7 @@ export class HexMapComponent implements OnInit, OnDestroy {
         } catch (e: any) {
             console.error('Error getting HexMap data from API ', e);
             console.info('Request params: ', params);
-            if(e.error && e.error[0]) {
+            if (e.error && e.error[0]) {
                 const errMessage = JSON.parse(e.error);
                 this.toastService.show({
                     title: 'Error while getting Hex Map data from API',
@@ -189,7 +197,7 @@ export class HexMapComponent implements OnInit, OnDestroy {
                     resolution: 6
                 }
                 this.updateLayer(reqParams)
-                return new H3HexagonLayer<HexDataType>({ id: 'temp' }); // Return temp layer, will be replaced
+                return new H3HexagonLayer<HexDataType>({id: 'temp'}); // Return temp layer, will be replaced
             } else if (num_of_cells * (7 * 7 * 7) < this.MAX_HEX_CELL) {
                 // Enable toggle even if not auto-switching
                 this.canToggleResolution = true;
@@ -197,12 +205,13 @@ export class HexMapComponent implements OnInit, OnDestroy {
         }
 
         // Build the layer
+        this.colorFunc = this.getColorFn();
         let layer = new H3HexagonLayer<HexDataType>({
             id: 'H3HexagonLayer',
             data: result,
             extruded: false,
             getHexagon: (d: HexDataType) => d.hex_cell,
-            getFillColor: this.getColorFn(),
+            getFillColor: this.colorFunc,
             pickable: true,
             opacity: 1,
         });
@@ -258,11 +267,11 @@ export class HexMapComponent implements OnInit, OnDestroy {
         // Positive color ranges from topicColor to blue
         const positiveInterpolator = interpolateHcl(topicLiteColorLch, "#313695");
 
-        const { min, max } = this.minMaxStats.result;
+        const {min, max} = this.minMaxStats.result;
         const abMax = Math.max(Math.abs(min), Math.abs(max));
-        const negativeScale = scalePow([-abMax, 0], [0, 1]).exponent(1/4);
-        const transparencyScale = scalePow([0, abMax], [0.3 * 255, 0.7 * 255]).exponent(1/4);
-        const positiveScale = scalePow([0, abMax], [0, 1]).exponent(1/4);
+        const negativeScale = scalePow([-abMax, 0], [0, 1]).exponent(1 / 4);
+        const transparencyScale = scalePow([0, abMax], [0.3 * 255, 0.7 * 255]).exponent(1 / 4);
+        const positiveScale = scalePow([0, abMax], [0, 1]).exponent(1 / 4);
 
         return (value: HexDataType): Color => {
             const result = value.result;

src/app/dashboard/hex-map/legend/legend.component.ts
@@ -11,7 +11,7 @@ import {HexDataType} from '../../types';
 export class LegendComponent implements OnChanges, AfterViewInit {
     @Input() minValue!: number;
     @Input() maxValue!: number;
-    @Input() colorFunction!: (value: HexDataType) => Color;
+    @Input() colorFunction!: ((value: HexDataType) => Color) | undefined;
     @Input() unit: string = '';
     @ViewChild('legendCanvas', {static: true}) canvasRef!: ElementRef<HTMLCanvasElement>;
 
@@ -33,6 +33,7 @@ export class LegendComponent implements OnChanges, AfterViewInit {
     }
 
     ngOnChanges(changes: SimpleChanges) {
+        console.log("yeet")
         if ((changes['minValue'] || changes['maxValue'] || changes['colorFunction'])
             && this.isCanvasReady) {
             this.drawLegend();