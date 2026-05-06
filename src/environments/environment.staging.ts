266de4b5c92ea9d4cf54299620d0be6d4de5e85f
jstier <stier.jochen@web.de>
2026-05-06 10:43:44 +0200

    config: adjust environment.staging.ts to use prod appwrite and tyk

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
     ohsomeStatsUrl: 'https://int-stats.now.ohsome.org',
-    ohsomeStatsServiceUrl: 'https://staging.api.heigit.org/ohsome-now-staging/v1',
-    appwriteProjectId: '66cef0a9000b0675681b',
-    appwriteEndpoint: 'https://staging.api.account.heigit.org',
-    accountFrontendUrl: 'https://staging.account.heigit.org',
+    ohsomeStatsServiceUrl: 'https://api.heigit.org/ohsome-now-staging/v1',
+    appwriteProjectId: '6751abc5002d7cdf3ab7',
+    appwriteEndpoint: 'https://api.account.heigit.org',
+    accountFrontendUrl: 'https://account.heigit.org',
     whosthatUrl: 'https://whosthat.osmz.ru/whosthat.php'
-
 };
