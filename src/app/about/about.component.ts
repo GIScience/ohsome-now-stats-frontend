36bae0207308883f30d31953f8830b8e4436aad4
jstier <stier.jochen@web.de>
2026-05-06 11:25:10 +0200

    feat: add section about tasking manager instances how to get their key

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {environment} from "@environments/environment";
 
 @Component({
     selector: 'app-about',
@@ -9,4 +10,5 @@ import { NgOptimizedImage } from '@angular/common';
 })
 export class AboutComponent {
 
+    protected readonly environment = environment;
 }
